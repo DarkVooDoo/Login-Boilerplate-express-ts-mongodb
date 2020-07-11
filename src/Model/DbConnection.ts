9e686aa7e6e0724beb34598ab5bacdd638aa57ba
DarkVooDoo <moisestazaro@gmail.com>
2020-07-11 20:11:55 +0200

    First Commit ☔☔

--- src/Model/DbConnection.ts
+++ src/Model/DbConnection.ts
@@ -4,7 +4,7 @@ import MongoClient from 'mongodb'
 
 export default async ()=>{
     try{
-        const uri = `YOUR MONGODB URL CONNECTION`
+        const uri = `mongodb+srv://NotifyClients:${process.env.APP_MONGODB_PASSWORD}.a9kz1.mongodb.net/Notifications?retryWrites=true&w=majority`
         const client = await MongoClient.connect(uri, {useUnifiedTopology: true})
         return client
     }catch(error) {console.log(error)}
